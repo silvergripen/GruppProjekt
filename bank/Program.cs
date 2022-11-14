abd844b7ddfcd15b2efca39e4fb1af2cd41ecb48
silvergripen <Kenneth.hedberg.molen@gmail.com>
2022-11-14 15:39:16 +0100

    Add project files.

---
+++ bank/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
