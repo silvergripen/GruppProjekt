47d4a59211f1d0c281414f1b79f969c4caa45656
silvergripen <Kenneth.hedberg.molen@gmail.com>
2022-11-15 11:34:26 +0100

    Har fixat så man kan logga in samt att man bara har 3 försök på sig

--- bank/Login.cs
+++ bank/Login.cs
@@ -11,26 +11,52 @@ namespace bank
         private Dictionary<string, int> userName = new Dictionary<string, int>();
         private Dictionary<string, string> userPassword = new Dictionary<string, string>();
         private Dictionary<string, bool> isAdmin = new Dictionary<string, bool>();
+        private int maxTries = 3;
+       
         public Login()
         {
             userName.Add("kenneth", 1);
             userPassword.Add("kenneth", "pass123");
             isAdmin.Add("kenneth", true);
+            
         }
         public bool ValidatePassword(string username, string password)
         {
-            return userPassword.Any(x => x.Key == username && x.Value == password);
+            return userPassword.Any(x => x.Key == username && x.Value == password); //Returnar en sann 
 
         }
+       
+        
         public void userInput()
         {
             Console.WriteLine("Skriv ditt användarnamn");
             var inputUName = Console.ReadLine();
-            Console.WriteLine("Skriv ditt lösenord");
-            var inputUPass = Console.ReadLine();
-            var isValid = ValidatePassword(inputUName, inputUPass);
-            Console.WriteLine(isValid? "Du är inloggad" : "Gissat fel");
-        }
+            
+            bool isValid;
 
+            do //En loop som gör så du bara har 3 försök på dig för att logga in.
+            {
+                Console.WriteLine("Skriv ditt lösenord");
+                var inputUPass = Console.ReadLine();
+                isValid = ValidatePassword(inputUName, inputUPass);
+                maxTries--;
+                Console.WriteLine(isValid ? "Du är inloggad" : "Gissat fel");
+                if(maxTries >= 1)
+                {
+                    Console.WriteLine(maxTries + " Försök kvarad");
+                }
+                
+            }
+            while (maxTries >= 1 && isValid == false);
+        }
+        public void CheckAdmin()// Kollar om isadmin har admin i sig. Början för att logga in som admin.
+        {
+            if (isAdmin.ContainsValue(true))
+            {
+                //Då är dom admin.
+            }
+        }
     }
 }
+        
+
