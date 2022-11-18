18fb333eaae6eedb687e9b25f2f89073a235413c
silvergripen <Kenneth.hedberg.molen@gmail.com>
2022-11-18 18:15:24 +0100

    början på change exchange

--- bank/Admin.cs
+++ bank/Admin.cs
@@ -9,12 +9,15 @@ namespace bank
         public Dictionary<string, UserInfo> User = new Dictionary<string, UserInfo>();
         string inputPersNm;
         string inputPass;
+        string input;
+        double tempValue;
+        
         public void AddCustomer()
         {
             Console.WriteLine("Vad är personnummeret på personen som du vill lägga till?");
-          inputPersNm = Console.ReadLine();
+          input = Console.ReadLine();
             Console.WriteLine("Vilket lösenord vill du ge till personen som ska logga in?");
-          inputPass = Console.ReadLine();
+          input = Console.ReadLine();
             User.Add(inputPersNm, new UserInfo
             {
                 passWord = inputPass,
@@ -22,7 +25,13 @@ namespace bank
             });
             
         }
-        public void 
+        //public void ChangeExchangeRate()
+        //{
+        //    Console.WriteLine("Vad ligger Euron på nu?");
+        //    input = Console.ReadLine();
+        //    tempValue = double.Parse(input, double);
+        //    dictExchangeRate["EUR"] = input;
+        //}
     }
 
 }
